1bc73b9b6994eb1b5a05e26ebe32bbe54db681a2
tabularasae <tabularasae@icloud.com>
2025-09-16 09:55:50 -0400

    final touches

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var app = express();
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/sum", function (req, res) {
-  let ans = parseFloat(req.query.num1) + parseFloat(req.query.num2);
-  res.status().send(ans.toString());
+app.get("/sum", (req, res) => {
+  const ans = parseFloat(req.query.num1) + parseFloat(req.query.num2);
+  res.send(ans.toString());
 });
-app.listen(3000, function () {
-  console.log("server is listening!!!");
+
+app.listen(3000,  () => {
+  console.log("server is running on http://localhost:3000");
 });
