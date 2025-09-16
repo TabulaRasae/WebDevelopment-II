888e7fe9c43f203a74ec32fbc0b89b65dab64c7a
tabularasae <tabularasae@icloud.com>
2025-09-16 09:37:52 -0400

    configured the app

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/sum.js", function (req, res) {
+app.get("/sum", function (req, res) {
   let ans = parseFloat(req.query.num1) + parseFloat(req.query.num2);
-  res.status(200);
-  res.send(ans.toString());
+  res.status().send(ans.toString());
 });
-
 app.listen(3000, function () {
-  coonsole.log("server is listening!!!");
+  console.log("server is listening!!!");
 });
